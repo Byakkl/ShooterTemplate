e8e734da3a5ce2fae097872d23e2269233665813
Travis <devbyakkl@gmail.com>
2024-04-04 16:57:41 -0400

    Added scriptable object to contain reference to GameController instance for dependency injection Updated GameController to track and update items Removed spawn point reference from GameController

--- Assets/Game Logic/Game Controller/GameController.cs
+++ Assets/Game Logic/Game Controller/GameController.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    //Spawn point for player prefab
     [SerializeField]
-    Transform spawnPoint;
+    SO_GameController gameController;
     
-    void Start()
+    //List of all active items
+    List<IItem> itemInstances = new List<IItem>();
+
+    void Awake()
     {
+        gameController.instance = this;
         //Lock the cursor to the center of the screen and hide it
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update(){
+        //Update active items
+        foreach(IItem item in itemInstances){
+            item.ItemUpdate();
+        }
+    }
+
+    public void AddItem(IItem a_item){
+        //Don't add duplicates
+        if(itemInstances.Contains(a_item))
+            return;
+        
+        itemInstances.Add(a_item);
+    }
+
+    public void RemoveItem(IItem a_item){
+        if(!itemInstances.Contains(a_item))
+            return;
+
+        itemInstances.Remove(a_item);
     }
 }
